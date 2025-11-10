697fb526088a4453da2a922e39804f47d7f8dc55
dengxihan <daniel19@sjtu.edu.cn>
2025-11-10 14:14:32 +0800

    update test

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -9,8 +9,11 @@ class MainTest {
 	@Test
 	void test() {
 		Main main = new Main();
-		int result = main.add(1, 0);
-		assertEquals(1, result);
+		int result1 = main.add(1, 0);
+		assertEquals(1, result1);
+		int result2 = main.add(-1, 1);
+		int result3 = main.add(1, -1);
+		int result4 = main.add(Integer.MAX_VALUE, 1);
 	}
 
 }
